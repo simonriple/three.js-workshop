3ba633c78e4c4da603883913be0c3fd653e01683
Lars Aarseth Berge <larsaarsethberge@Larss-MacBook-Pro.local>
2022-08-23 15:36:51 +0200

    Added planegeometry to scene with GLSL Shaders

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ const sizes = {
 //Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.x = 0;
-camera.position.y = 0;
-camera.position.z = 2;
+camera.position.y = 1;
+camera.position.z = 3;
 scene.add(camera);
 
 //Camera controls
@@ -49,19 +49,67 @@ const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 
+//Plane object
+const vertexShader = `
+uniform float time;
+uniform float frequency;
+uniform float amplitude;
+varying float height;
+
+void main() {
+    vec3 currentPosition = position;
+
+    height = sin(frequency * currentPosition.x + time);
+    
+    currentPosition.z = height*amplitude;
+
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(currentPosition, 1.0);
+}
+`;
+const fragmentShader = `
+uniform vec3 maxColor;
+uniform vec3 minColor;
+varying float height;
+
+void main() {
+    float normalizedHeight = (height+1.0)*0.5;
+    vec3 color = mix(minColor, maxColor, normalizedHeight);
+    gl_FragColor = vec4(color, 1);
+}
+`;
+
+const planeMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+        time: { type: "f", value: 0 },
+        frequency: { type: "f", value: 10 },
+        amplitude: { type: "f", value: .1 },
+        maxColor: {type: "vec3", value: new THREE.Vector3(1,1,1)},
+        minColor: {type: "vec3", value: new THREE.Vector3(0,0,1)}
+    },
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader
+});
+const planeGeometry = new THREE.PlaneGeometry(3,3,100,100);
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+planeMesh.rotation.x = -Math.PI/2;
+scene.add(planeMesh);
+
 var clock = new THREE.Clock();
 
 //Action / Animation
 function tick() {
 
-    const elapsedTime = clock.getElapsedTime()
+    const elapsedTime = clock.getElapsedTime();
     //Animation
-    mesh.rotation.y += .01
-    mesh.position.y = Math.sin(elapsedTime)
+    mesh.rotation.y += .01;
+    mesh.position.y = Math.sin(elapsedTime);
 
-    requestAnimationFrame(tick);
+    //Animate plane
+    planeMaterial.uniforms.time.value = elapsedTime;
+    
     control.update();
     renderer.render(scene, camera);
+    requestAnimationFrame(tick);
 };
 
 tick();
